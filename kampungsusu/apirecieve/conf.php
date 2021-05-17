3781fada8aea4f05b870a0ec44c45c7315bc36d2
Izzan Silmi Aziz <58686170+isa96@users.noreply.github.com>
2021-05-18 00:48:36 +0700

    Delete kampungsusu directory

--- kampungsusu/apirecieve/conf.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-// buat koneksi dengan MySQL, gunakan database: universitas
-$link = mysqli_connect("localhost", "root", "root", "kampungsusu");
-
- ?>
--- kampungsusu/application/views/template.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$page = $this->uri->segment(2);
-include('loadview/cssloader.php');
-include('loadview/headerloader.php');
-include('loadview/menuloader.php');
-include("$page".".php");
-include('loadview/footerloader.php');
-include('loadview/sidebarloader.php');
-include('loadview/jsloader.php');
